b0dcf16cff875530bcf5998ba0df1f6b27cd4ff5
Tolegen Timur <demigodkz@gmail.com>
2022-05-13 22:04:41 +0600

    feat: migrate to typescript "app" module

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,18 +1,16 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
+import getElem from '../../helpers/getElem';
+import { IResponseNews, IResponseSources } from '../../interfaces/interfacesAppView';
 
 class App {
-    constructor() {
-        this.controller = new AppController();
-        this.view = new AppView();
-    }
+    constructor(private controller = new AppController(), private view = new AppView()) {}
 
     start() {
-        document
-            .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-        this.controller.getSources((data) => this.view.drawSources(data));
+        getElem('.sources').addEventListener('click', (e) =>
+            this.controller.getNews<IResponseNews>(e, (data) => this.view.drawNews(data))
+        );
+        this.controller.getSources<IResponseSources>((data) => this.view.drawSources(data));
     }
 }
-
 export default App;
