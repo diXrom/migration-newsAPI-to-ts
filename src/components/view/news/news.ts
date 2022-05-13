7d1f81afb7e44926ac7908e4032c2f4501742a59
Tolegen Timur <demigodkz@gmail.com>
2022-05-13 21:41:16 +0600

    feat: add helper function isHTMLElem

--- src/components/view/news/news.ts
+++ src/components/view/news/news.ts
@@ -1,17 +1,17 @@
 import './news.css';
 import getElement from '../../../helpers/getElem';
 import INews from '../../../interfaces/interfaceNews';
+import isHTMLElem from '../../../helpers/isHTMLElem';
 
 class News {
     draw(data: INews[]) {
         const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
-
         const fragment = document.createDocumentFragment();
         const newsItemTemp = getElement<HTMLTemplateElement>('#newsItemTemp');
 
         news.forEach((item, idx) => {
-            const newsClone = <HTMLElement>newsItemTemp.content.cloneNode(true);
-
+            const newsClone = newsItemTemp.content.cloneNode(true);
+            if (!isHTMLElem(newsClone)) throw new Error(`This element is not HTMLElement`);
             if (idx % 2) getElement('.news__item', newsClone).classList.add('alt');
 
             getElement('.news__meta-photo', newsClone).style.backgroundImage = `url(${
@@ -29,10 +29,8 @@ class News {
             getElement('.news__read-more a', newsClone).setAttribute('href', item.url);
             fragment.append(newsClone);
         });
-
         getElement('.news').innerHTML = '';
         getElement('.news').appendChild(fragment);
     }
 }
-
 export default News;
--- src/components/view/sources/sources.ts
+++ src/components/view/sources/sources.ts
@@ -1,25 +1,23 @@
 import getElement from '../../../helpers/getElem';
+import isHTMLElem from '../../../helpers/isHTMLElem';
 import ISources from '../../../interfaces/interfaceSources';
 import './sources.css';
 
 class Sources {
     draw(data: ISources[]) {
         const sources = data.length >= 9 ? data.filter((_item, idx) => idx < 9) : data;
-
         const fragment = document.createDocumentFragment();
         const sourceItemTemp = getElement<HTMLTemplateElement>('#sourceItemTemp');
 
         sources.forEach((item) => {
-            const sourceClone = <HTMLDivElement>sourceItemTemp.content.cloneNode(true);
-
+            const sourceClone = sourceItemTemp.content.cloneNode(true);
+            if (!isHTMLElem(sourceClone)) throw new Error(`This element<sourceClone> is not HTMLElement`);
             getElement('.source__item-name', sourceClone).textContent = item.name;
             getElement('.source__item', sourceClone).setAttribute('data-source-id', item.id);
 
             fragment.append(sourceClone);
         });
-
         getElement('.sources').append(fragment);
     }
 }
-
 export default Sources;
